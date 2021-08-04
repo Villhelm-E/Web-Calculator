ffa0f16fe99c760437118c3505d2514cb475cf2c
villhelm-e <villhelm.e@gmail.com>
2021-08-04 13:27:04 -0700

    Got calculations working

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,27 @@ let operandA = 0;
 let operandB = 0;
 let calcValue = 0;
 let operationMode = "entry";
+let operatorMem = ""
+let displayValue = 0;
 
 //Takes two inputs, returns the sum
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 //Takes two inputs, returns the difference of a - b
 function subtract(a, b) {
-    return a - b;
+    return +a - +b;
 }
 
 //Takes two inputs, returns the product
 function multiply(a, b) {
-    return a * b;
+    return +a * +b;
 }
 
 //Takes two inputs, returns the quotient of a / b
 function divide(a, b) {
-    return a / b;
+    return +a / +b;
 }
 
 //Takes three inputs, the first input choose a mathematical operation to perform on the other two inputs
@@ -56,15 +58,19 @@ function typeToDisplay(str) {
     }
 }
 
+function clearDisplay(){
+    display.textContent = "";
+}
+
 //checks to see if the display is full
 function fullDisplay() {
     return display.textContent.length >= DISPLAY_LENGTH;
 }
 
 function chooseOperator(str) {
-    console.log(str);
     operationMode = str;
-    operandA = display.textContent;
+    operatorMem = str;
+    operandA = display.textContent.trim();
 }
 
 
@@ -72,16 +78,42 @@ function chooseOperator(str) {
 const num = document.querySelectorAll(".number");
 num.forEach(item => {
     item.addEventListener('click', () => {
-        typeToDisplay(item.textContent.trim());
-        console.log(operandA);
+        
+        if (operationMode == "entry") {
+            typeToDisplay(item.textContent.trim());
+        }
+        else {
+            operandA = display.textContent.trim();
+            clearDisplay()
+            operationMode = "entry";
+            typeToDisplay(item.textContent.trim())
+        }
+        
     });
 });
 
 const equals = document.querySelector("#equals");
 equals.addEventListener('click', () => {
-    switch (operationMode) {
-        case "addition":
-            operate("addition", a, b);
+    if (operationMode == "entry") {
+        console.log(equals.textContent)
+        operandB = display.textContent.trim();
+        console.log(operandA);
+        console.log(operandB);
+
+        switch (operatorMem){
+            case "addition":
+                display.textContent = add(operandA, operandB);
+                break;
+            case "subtraction":
+                display.textContent = subtract(operandA, operandB);
+                break;
+            case "multiply":
+                display.textContent = multiply(operandA, operandB);
+                break;
+            case "division":
+                display.textContent = divide(operandA, operandB);
+                break;
+        }
     }
 })
 
@@ -89,5 +121,6 @@ const operators = document.querySelectorAll(".operation");
 operators.forEach(item => {
     item.addEventListener('click', () => {
         chooseOperator(item.id);
+        displayValue = display.textContent.trim();
     });
 })
