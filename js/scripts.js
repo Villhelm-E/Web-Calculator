df7db2ddfe1842319c10ea167e9e605bb5ee011f
villhelm-e <villhelm.e@gmail.com>
2022-08-09 08:58:59 -0700

    Got some logic to work

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,8 @@ const OpDisText = document.querySelector("#OpDisText");
 const DISPLAY_LENGTH = 14;
 let operandA = 0;
 let operandB = 0;
-let operationMode = "entry";
+let displayVal = "";
+let operator = "";
 
 //...............................
 //Functions
@@ -57,32 +58,20 @@ function typeToDisplay(str) {
     //use case where user is entering brand new data
     console.log('%c TypeToDisplay()', 'color:#daba55');
     console.log("str = " + str);
-    console.log("typeToDisplay(" + display.textContent + ")");
+    console.log("Before: typeToDisplay(" + display.textContent + ")");
 
-    switch (operationMode) {
-        case "entry":
-            if (display.textContent == "0") {
-                display.textContent = str;
-            }
-            //use case where user is continuing to enter data
-            else {
-                display.textContent += str;
-            }
-            
-            break;
-        case "operation":
-            break;
-        case "finalized":
-            break;
+    if (display.textContent == "0") {
+        display.textContent = str;
     }
-
-    console.log("typeToDisplay(" + display.textContent + ")");
+    else{
+        display.textContent += str;
+    }
+    console.log("After: typeToDisplay(" + display.textContent + ")");
 }
 
 //resets the display to 0
 function clearDisplay(){
     display.textContent = "0";
-    console.clear();
 }
 
 //Rounds the output to fit within the display
@@ -107,36 +96,7 @@ function fullDisplay(num) {
 //removes the last character from the display and updates globals accordingly
 function backspace() {
     //normal backspace when user is typing a number
-    if (operationMode == "entry") {
-        //save display value to variable
-        let str = display.textContent.trim();
-        
-        //if there's only one digit on display
-        if (str.substring(0,str.length - 1) == "") {
-            display.textContent = 0;
-        }
-        //truncate last digit on display
-        else {
-            display.textContent = str.substring(0,str.length - 1);
-        }
-    }
-    //clear display and memory after user has evaluated a calculation
-    else if (operationMode == "entry") {
-        //clear every variable and reset the display
-        operandA = 0;
-        operandB = 0;
-        operationMode = "entry";
 
-        clearDisplay();
-    }
-    //Handle behavior where the user has pressed an operation button but
-    //hasn't chosen a second number
-    else {
-        //Forget the operation the user entered and go back to entry mode
-        OpDisText.textContent = "";
-        operationMode = "entry";
-        // operationMem = "";
-    }
 }
 
 function DisablePeriod(){
@@ -165,44 +125,12 @@ num.forEach(item => {
     item.addEventListener('click', () => {
         //Log
         console.log('%c .NUM', 'color:#bada55');
-        console.log("operationMode = " + operationMode);
-        
-        //Modes
-        switch(operationMode) {
-            //Entry mode
-            case "entry":   
-                console.log('%c Entry Case', 'color:#EF1012');
-                console.log(item.textContent.trim());
-                typeToDisplay(item.textContent.trim());
-                break;
-            //Finalized mode
-            case "finalized":
-                console.log('%c Finalized Case', 'color:#EF1012');
-                
-                //reset display and variables
-                operandA = 0;
-                operandB = 0;
-                display.textContent = item.textContent.trim();
-                
-                //then just like entry mode
-                typeToDisplay(item.textContent.trim());
-
-                //switch to entry mode
-                operationMode = 'entry';
-                
-                break;
-            //Operation mode
-            default:
-                console.log('%c Operation Case', 'color:#EF1012');
-                operandB = item.textContent.trim();
-                console.log("text content is " + item.textContent.trim());
-                console.log("operandA was " + operandA);
-                operandA = display.textContent.trim();
-                console.log("now it's " + operandA);
-                display.textContent = operandB;
-//                operationMode = 'entry';
-                break;
+        console.log(item.textContent);
+        if (operator != ""){
+            clearDisplay();
         }
+        
+        typeToDisplay(item.textContent.trim());
     });
 });
 
@@ -214,36 +142,10 @@ operators.forEach(item => {
     item.addEventListener('click', () => {
         //Log
         console.log('%c .OPERATION', 'color:#bada55');
-        console.log("item.id = " + item.id);
-        console.log("operationMode = " + operationMode)
-        
-        //Operation Mode
-        switch (operationMode) {
-            //Entry mode
-            case "entry":
-                console.log('%c Entry Case', 'color:#EF1012');
-                
-                console.log("operandA was " + operandA)
-                operandA = display.textContent.trim();
-                console.log("now it's " + operandA)
-                operationMode = item.id.trim();
-                console.log("operationMode = " + operationMode)
-                OpDisText.innerHTML = item.textContent.trim();
-            
-                break;
-            //Operation mode
-            case "operation":
-                console.log('%c Operation Case', 'color:#EF1012');
-                
-                break;
-            //Finalized mode
-            case "finalized":
-                console.log('%c Finalized Case', 'color:#EF1012');
-                
-                operandA = display.textContent.trim();
-                operationMode = item.id.trim();
-                OpDisText.innerHTML = item.textContent.trim();
-        }
+        operandA = display.textContent.trim();
+        console.log(item.id);
+        operator = item.id;
+        OpDisText.textContent = item.textContent.trim();
     });
 });
 
@@ -254,32 +156,13 @@ const equals = document.querySelector("#equals");
 equals.addEventListener('click', () => {    
     //Log
     console.log('%c #EQUALS', 'color:#bada55');
-    console.log("operationMode = " + operationMode);
+    console.log(operandA);
+    operandB = display.textContent.trim();
+    OpDisText.textContent = "";
+    display.textContent = "";
+    console.log(operandB);
+    typeToDisplay(operate(operator, operandA, operandB));
     
-    //Operation mode
-    switch (operationMode) {
-        //Entry mode
-        case "entry":
-            console.log('%c Entry Case', 'color:#EF1012');
-            
-            operandB = display.textContent.trim();
-            display.textContent = operate(operationMode, operandA, operandB);
-            OpDisText.textContent = "";
-            operationMode = "entry";
-            break;
-        //Finalized mode
-        case "finalized":
-            console.log('%c Finalized Case', 'color:#EF1012');
-
-            break;
-        default:
-            console.log('%c Operation Case', 'color:#EF1012');
-
-            operandB = display.textContent.trim();
-            display.textContent = operate(operationMode, operandA, operandB);
-            OpDisText.textContent = "";
-            operationMode = "finalized";
-    }
 });
 
 const period = document.querySelector("#period");
@@ -290,11 +173,11 @@ clearBtn.addEventListener('click', () => {
     //clear every variable and reset the display
     operandA = 0;
     operandB = 0;
-    operationMode = "entry";
 
     OpDisText.textContent = "";
 
     clearDisplay();
+    console.clear();
 });
 
 const backBtn = document.querySelector("#backspace");
